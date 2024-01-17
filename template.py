164b060e065d26a42f9839d7404e3c6275b98335
sunilgiri7 <seungiri841@gmail.com>
2024-01-17 00:24:37 -0800

    "folder structure"

--- template.py
+++ template.py
@@ -1,32 +1,23 @@
 import os
 from pathlib import Path
-import logging
+
+package_name = "mongodb_connect"
 
 list_of_files = [
-    ".github/workflows/.gitkeep",
-    "src/__init__.py",
-    "src/components/__init__.py",
-    "src/components/data_ingestion.py",
-    "src/components/data_transformation.py",
-    "src/components/model_trainer.py",  # Corrected the typo in the filename
-    "src/components/model_evaluation.py",
-    "src/pipeline/__init__.py",
-    "src/pipeline/training_pipeline.py",
-    "src/pipeline/prediction_pipeline.py",
-    "src/utils/__init__.py",
-    "src/utils/utils.py",
-    "src/logger/logging.py",  # Corrected the typo in the filename
-    "src/exception/exception.py",  # Corrected the missing file extension
-    "tests/unit/__init__.py",
-    "tests/integration/__init__.py",
-    "init_setup.sh",
-    "requirements.txt",
-    "requirements_dev.txt",
-    "setup.py",
-    "setup.cfg",
-    "pyproject.toml",
-    "tox.ini",
-    "experiment/experiment.ipynb"  # Corrected the typo in the filename
+   ".github/workflows/ci.yaml",
+   "src/__init__.py",
+   f"src/{package_name}/__init__.py", 
+   f"src/{package_name}/mongo_crud.py", 
+   "tests/__init__.py",
+   "tests/unit/__init__.py",
+   "tests/integration/__init__.py",
+   "init_setup.sh",
+   "requirements.txt", 
+   "setup.py",
+   "setup.cfg",
+   "pyproject.toml",
+   "tox.ini",
+   "experiments/experiments.ipynb", 
 ]
 
 for filepath in list_of_files:
@@ -34,6 +25,9 @@ for filepath in list_of_files:
     filedir, filename = os.path.split(filepath)
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
+
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath, "w"):
-            pass  # Corrected the indentation of this line
+        with open(filepath, "w") as f:
+            pass # create an empty file
+
+#its updated
